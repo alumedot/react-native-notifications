46c505669303b66f221fc49c42e3d682715a63c9
Alexey Kozlov <alumedot@icloud.com>
2020-09-12 13:20:47 +0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Button, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS).then(response => {
+      if (response.status !== 'granted') {
+        return Permissions.askAsync(Permissions.NOTIFICATIONS);
+      }
+      return response;
+    }).then(response => {
+      if (response.status !== 'granted') {
+        return;
+      }
+    });
+  }, []);
+
+  const triggerNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the first local notification we are sending',
+      },
+      trigger: {
+        seconds: 10,
+      }
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Trigger Notification" onPress={triggerNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
